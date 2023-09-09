c3dab5e5917d15fcfdf96f0627c14e426e8cd4ca
MatildaZh <clounadaprodolzhaetsa@gmail.com>
2023-09-09 17:37:13 +0300

    Добавил классы наследники от Factory

--- TPL13/Factory.h
+++ TPL13/Factory.h
@@ -1,1 +1,17 @@
 #pragma once
+
+class Factory {		//абстрактный базовый класс
+
+
+
+
+private:
+	Furniture furniture;
+
+
+
+public:
+
+
+
+};
---
+++ TPL13/Furniture.h
@@ -0,0 +1,5 @@
+#pragma once
+#include "Factory.h"
+class Furniture : Factory {
+
+};
